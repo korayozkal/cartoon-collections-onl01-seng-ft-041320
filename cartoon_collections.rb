83d2032009bdc3304491deb3da567725ac65d9ff
Koray Ozkal <koryozkal@gmail.com>
2020-04-23 05:47:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def summon_captain_planet(array)
    array.collect { |i| i.capitalize << "!" } 
 end
 
-
-end
+def long_planeteer_calls(calls)
+    calls.any? { |i| i.length > 4 }
+end	
 
 def find_the_cheese# code an argument here
   # the array below is here to help
