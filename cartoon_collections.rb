f27fed025a0e35d26708b88e56b8190b18eed02e
Koray Ozkal <koryozkal@gmail.com>
2020-04-23 05:47:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,6 @@ def long_planeteer_calls(calls)
     calls.any? { |i| i.length > 4 }
 end	
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+    array.find { |i| i == "cheddar" || i == "gouda" || i == "camembert" }
 end
